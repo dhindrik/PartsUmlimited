fe86c8a1c6800422c49dadc2f9ae6b77241446eb
Daniel Hindrikes <daniel@hindrikes.se>
2016-04-13 11:50:47 +0200

    searchview

---
+++ PartsUnlimited/PartsUnlimited/PartsUnlimited/ViewModels/SearchViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartsUnlimited.ViewModels
+{
+    public class SearchViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public SearchViewModel()
+        {
+            Items = new ObservableCollection<string>();
+        }
+
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private string _searchText;
+        public string SearchText
+        {
+            get
+            {
+                return _searchText;
+            }
+            set
+            {
+                _searchText = value;
+                RaisePropertyChanged();
+
+                if (_searchText.Length > 2)
+                {
+                    Search(); 
+                }
+            }
+        }
+
+        private void Search()
+        {
+            Items.Clear();
+
+            foreach (var item in _data.Where(x => x.Contains(_searchText)).ToList())
+            {
+                Items.Add(item);
+            };
+        }
+
+        public ObservableCollection<string> Items { get; set; }
+
+        private List<string> _data = new List<string>()
+        {
+            "aluminiumfälg 14\"", "plåtfälg 14\"", "aluminiumfälg 14\"","aluminiumfälg 15\"","aluminiumfälg 16\"", "plåtfälg 15\"", "plåtfälg 16\"", "plåtfälg 17\""
+        };
+    }
+}
--- PartsUnlimited/PartsUnlimited/PartsUnlimited/Views/SearchView.xaml.cs
+++ PartsUnlimited/PartsUnlimited/PartsUnlimited/Views/SearchView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PartsUnlimited.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,8 @@ namespace PartsUnlimited.Views
         public SearchView()
         {
             InitializeComponent();
+
+            BindingContext = new SearchViewModel();
         }
     }
 }
