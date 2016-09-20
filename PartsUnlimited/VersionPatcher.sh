82f13f828caee616b991fc340b97998cd0c82db1
Daniel Hindrikes <daniel@hindrikes.se>
2016-09-20 11:17:48 +0200

    versionpatcher

--- PartsUnlimited/VersionPatcher.sh
+++ PartsUnlimited/VersionPatcher.sh
@@ -4,5 +4,5 @@ echo "setting versions to 1.0.$BUILD_BUILDID"
 export PLISTBUDDY="/usr/libexec/PlistBuddy"
 
 export INFO=$BUILD_SOURCESDIRECTORY"/PartsUnlimited/PartsUnlimited/PartsUnlimited.iOS/Info.plist"
-$PLISTBUDDY $INFO -c "set :CFBundleShortVersionString 1.0.$BUILD_BUILDID"
-$PLISTBUDDY $INFO -c "set :CFBundleVersion 1.0"
+$PLISTBUDDY $INFO -c "set :CFBundleShortVersionString 1.0"
+$PLISTBUDDY $INFO -c "set :CFBundleVersion 1.0.$BUILD_BUILDID"
