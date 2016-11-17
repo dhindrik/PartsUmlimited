a83ab66dcf712c331832555f2f38c3b70c197fec
Daniel Hindrikes <daniel@hindrikes.se>
2016-11-17 12:35:59 +0100

    added ios test

--- PartsUnlimited/UITests/Tests.cs
+++ PartsUnlimited/UITests/Tests.cs
@@ -8,7 +8,6 @@ using Xamarin.UITest.Queries;
 namespace UITests
 {
     [TestFixture(Platform.Android)]
-    [TestFixture(Platform.iOS)]
     public class Tests
     {
         IApp app;
---
+++ PartsUnlimited/UITests/iOSTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITests
+{
+    [TestFixture(Platform.iOS)]
+
+    public class iOSTests
+    {
+        IApp app;
+        Platform platform;
+
+        public iOSTests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+      
+           [Test]
+        public void SearchAndBuy()
+        {
+            
+            app.Screenshot("Search");
+            
+            app.Tap(x => x.Text("Aluminum rim 14\""));
+          
+            app.Tap(x => x.Text("Buy"));
+           
+            app.Tap(x => x.Text("Cart"));
+            app.Screenshot("Cart");
+           
+        }
+
+    }
+}
+
+
